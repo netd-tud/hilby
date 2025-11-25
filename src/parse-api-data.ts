ec315502624365fee9d3be1d8eaf3590bf7281d4
Alex Maennel <alexmnnl@googlemail.com>
2025-11-25 15:26:37 +0100

    fix: update prefix parsing logic to handle /0 and /9 announcements.

--- src/parse-api-data.ts
+++ src/parse-api-data.ts
@@ -44,39 +44,50 @@ self.onmessage = function (e: DataInterface) {
         }
         const lookUpRaw = new Uint8Array(2**24);
         
-        const unique24s = new Set<number>();
-        
-        e.data.data.filter((v:string) => !v.includes(":")).forEach((prefix: string) => {
-        const [ip, mask] = prefix.split("/");
-        const netmask = parseInt(mask);
-        
-        const long = ip2long(ip);
-        const bitmask = 0xFFFFFFFF << (32 - netmask)
-        const base = (long  & bitmask & 0xFFFFFFFF ) >>>0;
-        
-        if (netmask < 24) {
-            
-            for (let i = 0; i < (2**(24 - netmask)); i++) {
-            unique24s.add(base + (i << 8));
-            }
-        } else {
-            unique24s.add(base);
-        }
-        });
-        
+        let unique24s = new Set<number>();
         const mapIndexes = Object.keys(lookUpMaps) as unknown as Array<keyof typeof lookUpMaps>;
-        
-        for (const prefix of unique24s) {
-        for (const i of mapIndexes) {
-            const base = long2ip(((prefix & (0xFFFFFFFF << (32 - i))) & 0xFFFFFFFF) >>>0) + `/${i}`;
-            if (base in lookUpMaps[i]) {
-            lookUpMaps[i][base] += 1;
-            } else {
-            lookUpMaps[i][base] = 1;
+        let current_first_octet = 0;
+        const filtered_prefixes = e.data.data.filter(v => parseInt(v.split("/")[1]) >= 8)
+        for (const prefix of filtered_prefixes.sort((a,b) => parseInt(a.split(".")[0]) - parseInt(b.split(".")[0]))) {
+            if (prefix.includes(":")) continue;
+
+            const [ip, mask] = prefix.split("/");
+            const netmask = parseInt(mask);
+            const first_octet = parseInt(ip.split(".")[0])
+            
+            if (first_octet > current_first_octet) {
+                for (const prefix of unique24s) {
+                    for (const i of mapIndexes) {
+                            const base = long2ip(((prefix & (0xFFFFFFFF << (32 - i))) & 0xFFFFFFFF) >>>0) + `/${i}`;
+                        if (base in lookUpMaps[i]) {
+                            lookUpMaps[i][base] += 1;
+                        } else {
+                            lookUpMaps[i][base] = 1;
+                        }
+                    } 
+                    lookUpRaw[prefix >>> 8] = 1;
+                }
+
+                unique24s = new Set<number>();
+                current_first_octet = first_octet;
             }
-        } 
-        lookUpRaw[prefix >>> 8] = 1;
+
+            const long = ip2long(ip);
+            const bitmask = 0xFFFFFFFF << (32 - netmask)
+            const base = (long  & bitmask & 0xFFFFFFFF ) >>>0;
+            
+            if (netmask < 24) {
+                
+                for (let i = 0; i < (2**(24 - netmask)); i++) {
+                    unique24s.add(base + (i << 8));
+                }
+            } else {
+                unique24s.add(base);
+            }
         }
+        
+        
+        
         
         self.postMessage({ ipv6: false, data: {maps: lookUpMaps, raw: lookUpRaw}});
     }
